d3ab1a2e81820194a89c6a012605b586c4ee4262
Clayton Stewart <claytons1212@gmail.com>
2023-12-13 09:29:39 -0700

    clean up imports

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 // import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import HomePage from './pages/HomePage.jsx';
-import PeakInfo from './pages/peakInfo.jsx';
+import HomePage from './pages/HomePage.jsx'
+import PeakInfo from './pages/peakInfo.jsx'
 
-import styles from './styles.css';
+import styles from './styles.css'
 
 //create app HTML structure
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/peakInfo" element={<PeakInfo />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
